cae362477b07741dc8444be2b7309ef9f03d5e10
Paul Ronga <paul.ronga@laposte.net>
2016-09-23 10:14:28 +0200

    Add localizations

---
+++ D3/js/lang.de.js
@@ -0,0 +1,9 @@
+// lang.de.js
+lang = {
+    greeting : "Hallo",
+    town : "Gemeinde",
+    singleMen : "Ledige Männer",
+    singleWomen : "Ledige Frauen",
+    in : "in",
+    
+};
---
+++ D3/js/lang.fr.js
@@ -0,0 +1,9 @@
+// lang.fr.js
+lang = {
+    greeting : "Salut",
+    town : "Commune",
+    singleMen : "Hommes célibataires",
+    singleWomen : "Femmes célibataires",
+    in : "à",
+
+};
